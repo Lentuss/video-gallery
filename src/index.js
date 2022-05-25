121fb4a8b05e2d0935a2bcb045de06a22a5c03e4
Olena Pohybil <itlentus@gmail.com>
2022-05-25 19:28:51 +0300

    error

--- src/index.js
+++ src/index.js
@@ -1,1 +1,15 @@
-import './sass/main.scss';
+import SimpleLightbox from "simplelightbox";
+import Notiflix, { Notify } from 'notiflix';
+import axios from "axios";
+import { searchQuery } from "./pixabay";
+
+import "simplelightbox/dist/simple-lightbox.min.css";
+import './css/styles.css';
+
+const form = document.querySelector("#search-form");
+const input = document.querySelector("input[searchQuery]");
+const submitBtn = document.querySelector("button[submit]");
+const gallery = document.querySelector(".gallery");
+const loadMoreBtn = document.querySelector(".load-more");
+
+console.log("hello");
---
+++ src/pixabay.js
@@ -0,0 +1,7 @@
+// export function searchQuery(query){
+//     const url = "https://pixabay.com/api/";
+//     const options = `?key=27641308-8655ee82f3657fe3b5b399be2&q=${query}&image_type=photo&orientation=horisontal&safesearch=true`;
+
+//     return fetch(`${url}${options}`)
+
+// }
