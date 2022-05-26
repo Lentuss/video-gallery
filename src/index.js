b4ea8fa25bcd446002d3696d58dcedaf75b6209d
Olena Pohybil <itlentus@gmail.com>
2022-05-27 01:29:13 +0300

    add axios

--- src/index.js
+++ src/index.js
@@ -1,15 +1,140 @@
+//плавный скролл
+//бесконечный скролл
+
 import SimpleLightbox from "simplelightbox";
 import Notiflix, { Notify } from 'notiflix';
-import axios from "axios";
-import { searchQuery } from "./pixabay";
+import { searchPics } from "./pixabay";
 
 import "simplelightbox/dist/simple-lightbox.min.css";
 import './css/styles.css';
+import simpleLightbox from "simplelightbox";
 
 const form = document.querySelector("#search-form");
-const input = document.querySelector("input[searchQuery]");
-const submitBtn = document.querySelector("button[submit]");
+const input = document.querySelector("input");
 const gallery = document.querySelector(".gallery");
 const loadMoreBtn = document.querySelector(".load-more");
+const backToTopBtn = document.querySelector(".back-to-top");
 
-console.log("hello");
+let page = 1;
+let query = "";
+let lightbox;
+
+loadMoreBtn.classList.add("isHidden");
+
+const createGallery = async () => {
+    let markup;
+
+    try {
+        const data = await searchPics(query, page)
+
+        if (page === 1 && data.totalHits) {
+            Notify.success(`Hooray! We found ${data.totalHits} images.`)
+        }
+
+        if (!data.totalHits) {
+            Notify.failure("Sorry, there are no images matching your search query. Please try again.")
+        }
+
+        else {
+            checkQuantity(data);
+        }
+
+        data.hits.map(({ webformatURL, largeImageURL, tags, likes, views, comments, downloads }) => {
+          
+            markup =
+                `<div class="photo-card">
+                    <div class="thumb">
+                        <a class="photo-link" href="${webformatURL}">
+                            <img src="${largeImageURL}" alt="${tags}" loading="lazy" height="260px"/>
+                        </a>
+                    </div>
+                    <div class="info">
+                        <p class="info-item">
+                            <b>Likes</b>
+                            <span>${likes}</span>
+                        </p>
+                        <p class="info-item">
+                            <b>Views</b>
+                            <span>${views}</span>
+                        </p>
+                        <p class="info-item">
+                            <b>Comments</b>
+                            <span>${comments}</span>
+                        </p>
+                        <p class="info-item">
+                            <b>Downloads</b>
+                            <span>${downloads}</span>
+                        </p>
+                    </div>
+                </div>`;
+                
+            gallery.insertAdjacentHTML("beforeend", markup);
+        })
+            addGallery()
+        
+    } catch (error) { Notify.failure("Oops") }
+}
+
+const checkQuantity = (data) => {  
+    let imageQuantity = data.hits.length;
+
+    if (imageQuantity >= 40) {           
+        loadMoreBtn.classList.remove("isHidden");
+
+    } else {
+        Notify.warning ("We're sorry, but you've reached the end of search results.");
+
+        loadMoreBtn.classList.add("isHidden");
+        backToTopBtn.classList.remove("isHidden");
+    }
+}
+
+const checkQuery = () => {
+    if (query == "") {
+        Notify.failure('Please, enter key word to find');
+
+    } else {
+        createGallery();
+    } 
+}
+
+const addGallery = () => {
+   lightbox = new SimpleLightbox('.gallery a', {
+            caption: true,
+            captionsData: 'alt',
+            captionDelay: 250,
+        }).refresh();
+}
+
+const onSubmit = (e)=>{
+    e.preventDefault()
+    
+    gallery.innerHTML = "";
+    query = input.value;
+    page = 1;
+
+    checkQuery();
+
+    backToTopBtn.classList.add("isHidden");
+    loadMoreBtn.classList.add("isHidden");
+    input.value = "";
+}
+    
+const onLoadMore = () => {
+    page += 1;
+    lightbox.destroy();
+
+    createGallery(query, page);
+}
+
+const imageOnClick = (e) => {
+    e.preventDefault();
+
+    if (e.target.nodeName !== "IMG") {
+        return
+    }
+}
+
+form.addEventListener("submit", onSubmit)
+loadMoreBtn.addEventListener("click", onLoadMore);
+gallery.addEventListener("click",imageOnClick)
--- src/pixabay.js
+++ src/pixabay.js
@@ -1,7 +1,10 @@
-// export function searchQuery(query){
-//     const url = "https://pixabay.com/api/";
-//     const options = `?key=27641308-8655ee82f3657fe3b5b399be2&q=${query}&image_type=photo&orientation=horisontal&safesearch=true`;
+// import axios from "axios";
 
-//     return fetch(`${url}${options}`)
+const API_KEY = "27641308-8655ee82f3657fe3b5b399be2";
+const URL = "https://pixabay.com/api/";
 
-// }
+export async function searchPics(query, page) {
+    const options = `?key=${API_KEY}&q=${query}&page=${page}&per_page=40&image_type=photo&orientation=horizontal&safesearch=true`;
+    const search = await axios.get(`${URL}${options}`).then(({ data }) => data);
+    return search
+}
